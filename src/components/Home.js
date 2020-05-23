97c078014a9d368b2ad34af2e46e50ff60e615d4
Student_GA106 <student_ga106@Student-GA106.local>
2020-05-23 14:13:16 -0400

    first commit after what should have been 50 commits

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+export default class Home extends React.Component{
+  
+  render(){
+
+  
+    return (
+    <div className="App">
+     <div id="navBar">
+         <div id="logoDiv">Logo</div>
+         <div id="navItems">
+             <ul id="navList">
+                <div id="navHomeLink"><li>Home</li></div>
+                 <li id="navItemsThatHover">Goals</li>
+                 <li id="navItemsThatHover">Affirmation</li>
+                 <li id="navItemsThatHover">Journal</li>
+                 <li id="navItemsThatHover">Meditate</li>
+                 <li id="navItemsThatHover">VisionBoard</li>
+             </ul>
+         </div>
+     </div>
+     <div id="pageBackground"></div>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+    
+
+     <div id="homeBoxes">efwgaewa</div>
+     <div id="homeBoxes">ethesj5ej</div>
+     <div id="homeBoxes">b5ny5 w</div>
+    </div>
+  );
+    }
+}
